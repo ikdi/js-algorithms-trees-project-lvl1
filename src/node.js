a11dcb0b38eeb7180a0b9b5797b305fc6e7c71ca
KDI <perecd@ukr.net>
2022-01-11 21:40:38 +0200

    refactor to reduce complexity #3

--- src/node.js
+++ src/node.js
@@ -29,35 +29,44 @@ export default class Node {
     }
   }
 
+  findChild(segments, method) {
+    const [segment, ...rest] = segments;
+    if (!this.children.has(segment)) return null;
+
+    const child = this.children.get(segment);
+    const data = child.find(rest, method);
+    return data
+      ? { ...data, path: [segment, ...data.path] }
+      : null;
+  }
+
+  findDynChild(segments, method) {
+    const [segment, ...rest] = segments;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [dynSegment, child] of this.dynamicChildren.entries()) {
+      const data = child.find(rest, method);
+      if (data !== null) {
+        const name = dynSegment.slice(1);
+        return {
+          ...data,
+          path: [dynSegment, ...data.path],
+          params: { [name]: segment, ...data.params },
+        };
+      }
+    }
+
+    return null;
+  }
+
   find(segments, method) {
     if (segments.length === 0) {
       const handler = this.handlers[method];
       return handler ? { path: [], params: {}, handler } : null;
     }
 
-    const [segment, ...rest] = segments;
+    const data = this.findChild(segments, method);
+    if (data) return data;
 
-    if (this.children.has(segment)) {
-      const childNode = this.children.get(segment);
-      const childData = childNode.find(rest, method);
-      return childData
-        ? { ...childData, path: [segment, ...childData.path] }
-        : null;
-    }
-
-    // eslint-disable-next-line no-restricted-syntax
-    for (const [dynSegment, childNode] of this.dynamicChildren.entries()) {
-      const childData = childNode.find(rest, method);
-      if (childData !== null) {
-        const name = dynSegment.slice(1);
-        return {
-          ...childData,
-          path: [dynSegment, ...childData.path],
-          params: { [name]: segment, ...childData.params },
-        };
-      }
-    }
-
-    return null;
+    return this.findDynChild(segments, method);
   }
 }
